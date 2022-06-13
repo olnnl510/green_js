0c7c03eed6718345b0655f7ed08b0c8d6de310d2
olnnl510 <101864135+olnnl510@users.noreply.github.com>
2022-06-13 18:05:31 +0900

    Add files via upload

---
+++ js2/ch07/iconEditor copy 2.php
@@ -0,0 +1,41 @@
+<?php
+print"<table>";
+
+for($i=0; $i<5; $i++) {
+    print"<tr>";
+
+    for($z=0; $z<5; $z++) {
+        print"<td>hello</td>";
+
+    }
+    print"</tr>";
+
+}
+print"</table>";
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        table {
+            border-collapse: collapse;
+        }
+
+        td{
+            padding: 10px;
+            border: 1px solid #000;
+        }
+    </style>
+</head>
+<body>
+    
+</body>
+</html>
